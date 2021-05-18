8d447a74b65431696ed10bcd94b141579bcf6339
Robert Carter <kakulo83@gmail.com>
2021-05-18 00:49:10 -0700

    Added org-download, deft, beautified org-mode

--- init.el
+++ init.el
@@ -11,11 +11,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(gnutls-algorithm-priority "normal:-vers-tls1.3")
+ '(org-hidden-keywords '(title))
  '(package-selected-packages
-	 '(ivy-rich marginalia pdf-tools orderless treemacs-icons-dired lsp-ui evil-leader popwin treemacs-evil treemacs counsel tron-legacy-theme alect-themes org-bullets go-mode projectile lsp-mode org-roam org evil)))
+	 '(deft evil-collection org-download org-roam-server magit ivy-rich marginalia pdf-tools orderless treemacs-icons-dired lsp-ui evil-leader popwin treemacs-evil treemacs counsel tron-legacy-theme alect-themes org-bullets go-mode projectile lsp-mode org-roam org evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 6.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 2.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo")))))
