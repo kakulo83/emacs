2e2f98fdac4d6bacd82f4adc6e54ecd4a7a6fa51
Robert Carter <robert@onesignal.com>
2021-06-02 18:38:17 -0700

    Add lsp, yasnippet, companybox, org
    with working autocomplete

--- init.el
+++ init.el
@@ -10,13 +10,17 @@
 ;;; https://github.com/dajva/rg.el (ripgrep integration)
 ;;; https://luca.cambiaghi.me/vanilla-emacs/readme.html#h:6E4E5BD6-1930-4DCE-8E26-5ADAC2B9A152   (has some packages listed worth investigating)
 ;;; https://www.reddit.com/r/emacs/comments/gr72by/how_do_you_guys_refine_search_results_doom_emacs/ (small discussion on how to use ripgrep for searching)
-;;;
+;;; https://www.youtube.com/watch?v=AaUlOH4GTCs (simple tutorial on how to add custom ivy action options... when you press alt-o to show additional options for an ivy list)
+;;; https://www.reddit.com/r/emacs/comments/kqutap/selectrum_prescient_consult_embark_getting_started/gi6yibq/     INVESTIGATE THESE
+;;; 
 ;;; TODO
-;;;  - convenient git-blame
-;;;  - setup Tramp mode to interact with Staging/Production
 ;;;  - add keybinding to move to next/previous diagnostic error
+;;;  - look into this:  https://github.com/ericdanan/counsel-projectile    (use ivy for projectile commands... this should allow the use of ctrl-o within an ivy-list and show additional actions
 ;;;
 ;;; NOTE
+;;;
+;;;   In order to present additional actions when acting on a selection, for instance "find-file" (ctrl-x ctrl-f) you have to invoke the function 'counsel-find-file'.
+;;;   Then you trigger the UI for the additional actions with ctrl-o
 ;;;
 ;;; With counsel-rg you can pass any flags to ripgrep after --
 ;;;
@@ -43,13 +47,21 @@
  '(help-at-pt-display-when-idle '(flymake-diagnostic) nil (help-at-pt))
  '(help-at-pt-timer-delay 0.1)
  '(package-selected-packages
-	 '(counsel simple-modeline vimish-fold imenu-list lsp-ui typescript-mode lsp-mode go-mode bug-hunter use-package all-the-icons-dired tron-legacy-theme evil)))
+	 '(company-box yasnippet org embark counsel simple-modeline vimish-fold imenu-list lsp-ui typescript-mode lsp-mode go-mode bug-hunter use-package all-the-icons-dired evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 6.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 2.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo")))))
 
 (setq gc-cons-threshold 100000000) ;; increase garbage-collection threshold to 100mb from 8kb
 (setq evil-want-C-u-scroll t)
@@ -69,12 +81,12 @@
   :init
 	(setq evil-want-keybinding nil)
 	:config
+	(define-key evil-normal-state-map (kbd "-") 'dired)
 	(define-key evil-motion-state-map (kbd "C-z") nil)
 	(define-key evil-normal-state-map (kbd "C-s") 'projectile-switch-project)
 	(define-key evil-normal-state-map (kbd "C-n") 'neotree-toggle)
 	(define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file)
 	(define-key evil-normal-state-map (kbd "C-b") 'ivy-switch-buffer)
-	(define-key evil-normal-state-map (kbd "-") 'dired)
 	(define-key evil-normal-state-map (kbd "C-t") 'centaur-tabs-counsel-switch-group)
 	(define-key evil-normal-state-map (kbd "C-f") 'counsel-rg)
 	(define-key evil-normal-state-map (kbd "z-c") 'hs-hide-block)
@@ -85,14 +97,10 @@
 	:after evil
 	:ensure t
 	:config
+	(setq evil-collection-company-use-tng nil)
 	(evil-collection-init))
 
 (use-package all-the-icons)
-
-(use-package org-bullets
-  :init
-  (setq inhibit-compacting-font-caches t)
-  (add-hook 'org-mode-hook #'org-bullets-mode))
 
 (use-package ivy
   :init
@@ -111,14 +119,13 @@
   (popwin-mode 1))
 
 (use-package lsp-mode
+	:commands (lsp lsp-deferred)
   :init
   (setq lsp-keymap-prefix "C-c l")
 	(setq lsp-headerline-breadcrumb-enable nil)
-  :hook (go-mode . lsp-deferred)
-	:hook (typescript-mode . lsp-deferred)
-	:hook (ruby-mode . lsp-deferred)
-	:config
-  :commands lsp)
+  :hook (ruby-mode . lsp-deferred)
+	:bind (:map lsp-mode-map
+							("TAB" . completion-at-point)))
 
 (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
 
@@ -160,7 +167,7 @@
 	 "r" 'lsp-find-references
 	 "f" 'lsp-find-definition
 	 "d" 'flycheck-list-errors
-	 "gl" 'magit-log
+	 "gl" 'magit-log-all
 	 "gb" 'magit-blame
 	 "gs" 'magit-status
 	 "gp" 'magit-pull
@@ -170,6 +177,17 @@
 (use-package docker
 	:ensure t
 	:bind ("C-c d" . docker))
+
+(use-package go-mode
+	:ensure t
+	:hook ((go-mode . lsp-deferred)))
+
+(use-package typescript-mode
+	:mode ("\\.ts\\'" "\\.tsx\\'")
+	:hook (typescript-mode . lsp-deferred)
+	:config
+	(setq typescript-indent-level 2))
+
 
 (use-package flycheck
 	:ensure t
@@ -194,12 +212,11 @@
 				("g T" . centaur-tabs-backward)))
 
 (use-package doom-themes
-	:ensure t
-	:config
-	(setq doom-themes-enable-bolt t
-				doom-themes-enable-italic t)
-	(load-theme 'doom-wilmersdorf t)
-	(doom-themes-neotree-config))
+ 	:ensure t
+ 	:config
+ 	(setq doom-themes-enable-bolt t
+ 				doom-themes-enable-italic t)
+ 	(load-theme 'doom-one t)) ;; doom-nord  doom-wilmersdorf  doom-city-lights
 
 (use-package simple-modeline
 	:ensure t
@@ -210,12 +227,51 @@
   :diminish hs-minor-mode
   :hook (prog-mode  . hs-minor-mode))
 
+;;(use-package yasnippet
+;;	:ensure t
+;;  :diminish yas-minor-mode
+;;  :preface (defvar tmp/company-point nil)
+;;  :config
+;;  (yas-global-mode +1)
+;;  (advice-add 'company-complete-common
+;;              :before
+;;              #'(lambda ()
+;;                  (setq tmp/company-point (point))))
+;;  (advice-add 'company-complete-common
+;;              :after
+;;              #'(lambda ()
+;;                  (when (equal tmp/company-point (point))
+;;                    (yas-expand)))))
+
+(use-package company
+	:ensure t
+	:config (progn
+            ;; don't add any dely before trying to complete thing being typed
+            ;; the call/response to gopls is asynchronous so this should have little
+            ;; to no affect on edit latency
+            (setq company-idle-delay 0)
+            ;; start completing after a single character instead of 3
+            (setq company-minimum-prefix-length 1)
+            ;; align fields in completions
+            (setq company-tooltip-align-annotations t)))
+
+(use-package company-box
+	:ensure t
+  :hook (company-mode . company-box-mode))
+
+
+(use-package org
+	:ensure t
+	:config
+	:hook (prog-mode . yas-minor-mode))
+
 ;; HELP FUCNTIONS ========================================================================================================================================================================================================================================
 
 (defun unique-shell ()
 	"Create a new named shell buffer."
   (interactive)
-  (call-interactively 'shell)
+  (call-interactively 'term)
+  ;;(call-interactively 'shell)
   (rename-buffer (read-string "Enter buffer name: ")))
 
 (global-set-key (kbd "C-z") #'unique-shell)
@@ -253,58 +309,55 @@
 (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
 (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
 
-;; MODES =================================================================================================================================================================================================================================================
+(define-key evil-normal-state-map (kbd "C-c k") 'centaur-tabs--kill-this-buffer-dont-ask)
 
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
-;; NOTE: lsp in typescript is not watching all the directories for OneSignal because that is over 2k directories and
-;; would likely slow Emacs down
-;; You can configure this warning with the 'lsp-enable-file-watchers' and 'lsp-file-watch-threshold' variables
-;; there is also 'lsp-file-watch-ignored'
+;; MODES =================================================================================================================================================================================================================================================
 
 (show-paren-mode 1)
 
 ;; =======================================================================================================================================================================================================================================================
 
-;; Disable Toolbar
-(tool-bar-mode -1)
+(tool-bar-mode -1) ;; Disable Toolbar
 
-;; Make fullscreen
-(add-hook 'emacs-startup-hook 'toggle-frame-maximized)
+(add-hook 'emacs-startup-hook 'toggle-frame-maximized) ;; Make fullscreen
 
-;; Disable splash screen
-(setq inhibit-splash-screen t)
+(setq inhibit-splash-screen t) ;; Disable splash screen
 
-;; Disable scratch screen
-(setq initial-major-mode (quote fundamental-mode))
+(setq initial-major-mode (quote fundamental-mode)) ;; Disable scratch screen
 
-;; silence alert chimes
-(setq ring-bell-function 'ignore)
+(setq ring-bell-function 'ignore) ;; silence alert chimes
 
-;; Enable syntax highlighting
-(global-font-lock-mode 1)
+(global-font-lock-mode 1) ;; Enable syntax highlighting
 
-;; Don't wrap lines
-(set-default 'truncate-lines t)
+(set-default 'truncate-lines t) ;; Don't wrap lines
 
-;; Don't show scroll bars
-(toggle-scroll-bar -1)
+(toggle-scroll-bar -1) ;; Don't show scroll bars
 
-;; Show number lines
-(global-linum-mode t)
+(global-linum-mode t) ;; Show number lines
 
-;; add icons to dired using all-the-icons-dired-mode
-(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+(add-hook 'dired-mode-hook 'all-the-icons-dired-mode) ;; add icons to dired using all-the-icons-dired-mode
 
-;; Smoother scrolling with smaller steps
 (setq scroll-step            1
-      scroll-conservatively  10000)
+      scroll-conservatively  10000) ;; Smoother scrolling with smaller steps
 
-;; Focus cursor in new split buffers
 (defadvice split-window (after move-point-to-new-window activate)
   "Move the point to the newly created window after splitting."
-  (other-window 1))
+  (other-window 1)) ;; Focus cursor in new split buffers
 
-;; Set tab space to 2
-(setq-default tab-width 2)
+(setq-default tab-width 2) ;; Set tab space to 2
+
+(setq org-hidden-keywords '(title))
+
+(set-frame-font "Monaco-12" nil t)
+
+(setq-default explicit-shell-file-name "/bin/zsh")
+
+(setq lsp-enable-links nil)
+
+;;(setq lsp-gopls-codelens nil)
+
+;; =======================================================================================================================================================================================================================================================
+
+
 
 ;;; init ends here
