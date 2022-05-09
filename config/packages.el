edb30b2a027e63e054129995479a04e41b02365f
Robert Carter <kakulo83@gmail.com>
2022-05-08 19:05:20 -0700

    add embark actions for ace-window splits

--- config/packages.el
+++ config/packages.el
@@ -343,6 +343,19 @@
 	:init
 	:config
 	(setq aw-dispatch-always t)
+  (eval-when-compile
+  (defmacro my/embark-ace-action (fn)
+    `(defun ,(intern (concat "my/embark-ace-" (symbol-name fn))) ()
+       (interactive)
+       (with-demoted-errors "%s"
+         (require 'ace-window)
+         (let ((aw-dispatch-always t))
+           (aw-switch-to-window (aw-select nil))
+           (call-interactively (symbol-function ',fn)))))))
+  (define-key embark-file-map     (kbd "o") (my/embark-ace-action find-file))
+  (define-key embark-buffer-map   (kbd "o") (my/embark-ace-action switch-to-buffer))
+  (define-key embark-bookmark-map (kbd "o") (my/embark-ace-action bookmark-jump))
+
 	(eval-when-compile
   (defmacro my/embark-split-action (fn split-type)
     `(defun ,(intern (concat "my/embark-"
