01c50ddce84341b6c6ce646a7f1532d735786855
Robert Carter <kakulo83@gmail.com>
2022-05-08 20:16:24 -0700

    cleanup init, remove defadvice definitions

--- init.el
+++ init.el
@@ -55,18 +55,6 @@
 	(when (file-exists-p fountain-scripts)
 		(load-file fountain-scripts)))
 
-
-(defadvice split-window (after move-point-to-new-window activate)
-  "Move the point to the newly created window after splitting."
-  (other-window 1)) ;; Focus cursor in new split buffers
-
-
-;; allows vim '*' to grab words with underscores:  i.e   match_this_whole_thing
-(with-eval-after-load 'evil
-    (defalias #'forward-evil-word #'forward-evil-symbol)
-    ;; make evil-search-word look for symbol rather than word boundaries
-    (setq-default evil-symbol-word-search t))
-
 ;; https://www.gnu.org/software/emacs/manual/html_node/eshell/Input_002fOutput.html#Input_002fOutput 
 ;;(add-to-list 'eshell-visual-commands "git")
 ;;(add-to-list 'eshell-visual-commands "rails")
