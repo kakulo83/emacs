fd5e4e418c0921eef2cf431c8d18e4f5c472f2e0
Robert Carter <kakulo83@gmail.com>
2022-10-31 23:48:22 -0700

    add new TODO items etc

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 ;;; DEPENDENCIES
 ;;; MacTex
 ;;; nerd-fonts
+;;; Roboto Mono font
 ;;; ripgrep
 ;;; pngpaste
 ;;; pgformatter
@@ -16,40 +17,49 @@
 
 
 ;;; TODO
+;;;  - Add ability display Ripgrep results in new vertical/horizontal split via an Embark action
 ;;;  - Figure out how to use snippets in vterm
+;;;  - Investigate if a Yasnippet can be saved into a register, if it can then it might be possible to
+;;;      load a register with the contents of a snippet and paste that into Vterm (instead of having to save to a temp buffer)
 ;;;  - Figure out how to configure LSP for projects (ignore node-modules etc)
 ;;;  - When opening a new projectile project, set emacs current working directory to root of project
-;;;  - Add ability display Ripgrep results in new vertical/horizontal split via an Embark action
 ;;;  - Make Embark-Collect default to showing results in a vertical split
 ;;;  - Create a command "repl" that lets me interactively select
 ;;;    a programming language REPL (i.e python, elisp, etc)
 ;;;  - Find out if buffers like magit-diff/status etc can be ignored by perspective
 ;;;      There is no value/reason for them to show up in the buffer-list
-;;;  - Add an Embark identifier action to open a filepath from vterm into a regular Buffer (open the file in other words)
 ;;;  - Investigate:  https://github.com/jixiuf/vterm-toggle
 ;;;      This might be a solution to use evil to edit vterm commands etc
-;;;  - Investigate:  https://github.com/akermu/emacs-libvterm/issues/313#issuecomment-738842507
-;;;      Maybe some of these functions can be used to use evil in vterm commandline
 ;;;  - Add ability to select visual region in Org mode and create a new note from the region's contents
 ;;;      Simultaneously delete the selected content and replace it with a link to the new Org note
 ;;;  - Create my own theme
 ;;;      Use font from: https://www.labri.fr/perso/nrougier/GTD/index.html
 ;;;      Match grey colors from:  https://github.com/rougier/nano-emacs
-;;;  - Investigate if a Yasnippet can be saved into a register, if it can then it might be possible to
-;;;      load a register with the contents of a snippet and paste that into Vterm (instead of having to save to a temp buffer)
 ;;;  - create Embark Action on an Identifier to perform a ripgrep search/export action
 ;;;  - create Embark Action on perspective-list <leader>p to kill a perspective from the mini-buffer directly
 ;;;  - dig for gems here:  https://www.reddit.com/r/emacs/comments/nr3cxv/what_are_your_very_useful_emacs_key_bindings_fast/
 ;;;  - Create my own modeline, extract from: https://github.com/jessiehildebrandt/mood-line/blob/master/mood-line.el
-;;;
-;;;  - Investigate my magit status bug:  https://github.com/magit/magit/issues/4744
-;;;    https://github.com/magit/magit/issues/4739
-;;;  
 ;;;  - Searching by Tags in Notes should be orderless:
 ;;;      example:  if searching for ec2, aws, breezeway, any order should match
 ;;;
+;;;  TO LEARN
+;;;  - learn how to use Embarks Export for grep results and how to perform additional actions like wgrep
+;;;  - learn grep-mode
+;;;
+;;;  SNIPPETS TO TRY
+;;;
 ;;;  - GREAT EXAMPLE:   https://www.reddit.com/r/emacs/comments/ovkyov/vterm_completion_for_files_directories_command/
 ;;;
+;;;  - Investigate my magit status bug:  https://github.com/magit/magit/issues/4744
+;;;                                      https://github.com/magit/magit/issues/4739
+;;;  - investigate https://issuecloser.com/blog/vterm-completion-for-files-directories-command-history-and-programs-in-emacs
+;;;  - investigate this emacs config:  https://ladicle.com/post/config/#doom-dracula-theme-modeline
+;;;  - investigate this dudes config: https://github.com/jakebox/jake-emacs
+;;;      - he uses the cape package and corfu
+;;;  - Investigate:  https://github.com/akermu/emacs-libvterm/issues/313#issuecomment-738842507
+;;;      Maybe some of these functions can be used to use evil in vterm commandline
+;;;
+;;;  PACKAGES TO CONSIDER
 ;;;  - consider:  creating a documentation system within emacs (https://hynek.me/articles/productive-fruit-fly-programmer/)
 ;;;  - consider:  https://elpa.gnu.org/packages/devdocs.html
 ;;;  - consider:  https://github.com/dash-docs-el/dash-docs
@@ -60,10 +70,6 @@
 ;;;  - consider:  https://github.com/lassik/emacs-format-all-the-code
 ;;;  - consider:  https://www.reddit.com/r/emacs/comments/ovkyov/vterm_completion_for_files_directories_command/
 ;;;  - consider:  https://www.emacswiki.org/emacs/AutoInsertMode
-;;;  - investigate https://issuecloser.com/blog/vterm-completion-for-files-directories-command-history-and-programs-in-emacs
-;;;  - investigate this emacs config:  https://ladicle.com/post/config/#doom-dracula-theme-modeline
-;;;  - investigate this dudes config: https://github.com/jakebox/jake-emacs
-;;;      - he uses the cape package and corfu
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
