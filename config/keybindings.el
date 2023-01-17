315eb90c8fb16f4300b889f0c56b743526da488e
Robert Carter <kakulo83@gmail.com>
2023-01-16 21:58:59 -0800

    try out consult enhanced buffer switching
    interestingly, consult-project-buffer also seems to respect
    perspective scope.  And it has the added benefit of allowing
    the preview of recent buffers

--- config/keybindings.el
+++ config/keybindings.el
@@ -24,7 +24,7 @@
 (define-key evil-normal-state-map (kbd "-") 'dired)
 (define-key evil-normal-state-map (kbd "C-s") 'projectile-persp-switch-project)
 (define-key evil-normal-state-map (kbd "C-p") 'project-find-file)
-(define-key evil-normal-state-map (kbd "C-b") 'persp-switch-to-buffer*)
+(define-key evil-normal-state-map (kbd "C-b") 'consult-project-buffer)  ; 'persp-switch-to-buffer*)
 (define-key evil-normal-state-map (kbd "C-S-b") 'list-buffers)
 (define-key evil-normal-state-map (kbd "C-n") 'dired-sidebar-toggle-sidebar)
 (define-key evil-normal-state-map (kbd "C-c n") 'org-roam-capture)
