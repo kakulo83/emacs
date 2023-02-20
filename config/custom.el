cf942c309fc9de5e546917e0a93e78ba41a7b679
Robert Carter <kakulo83@gmail.com>
2023-02-20 10:07:45 -0800

    update custom file

--- config/custom.el
+++ config/custom.el
@@ -8,12 +8,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-	 '("de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0" "db5b906ccc66db25ccd23fc531a213a1afb500d717125d526d8ff67df768f2fc" "e87f48ec4aebdca07bb865b90088eb28ae4b286ee8473aadb39213d361d0c45f" default))
+	 '("d43860349c9f7a5b96a090ecf5f698ff23a8eb49cd1e5c8a83bb2068f24ea563" "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0" "db5b906ccc66db25ccd23fc531a213a1afb500d717125d526d8ff67df768f2fc" "e87f48ec4aebdca07bb865b90088eb28ae4b286ee8473aadb39213d361d0c45f" default))
  '(eshell-syntax-highlighting-global-mode t)
- '(org-drill-scope
-	 '("/Users/robertcarter/Notes/org-roam-notes/20211011060540-golang.org"))
  '(package-selected-packages
-	 '(eshell-syntax-highlighting kind-icon all-the-icons-completion nyan-mode eshell-git-prompt python-pytest paredit blackout nano-theme git-timemachine magit focus lsp-ui lox-mode prettier-js perspective typescript-mode tsx-mode modus-themes wgrep imenu-list side-hustle lsp-pyright python-black iceberg-theme web-mode corfu material-theme yafolding yasnippet-snippets use-package undo-tree tron-legacy-theme tree-sitter-langs sublime-themes sqlformat rg restclient rake rainbow-delimiters projectile pdf-view-restore origami org-roam-ui org-drill org-download org-bullets orderless olivetti native-complete multi-vterm marginalia kubel inflections inf-ruby helpful go-mode flycheck exec-path-from-shell evil-vimish-fold evil-leader evil-collection enh-ruby-mode embark-consult doom-themes doom-modeline docker dired-sidebar dashboard bug-hunter balanced-windows auctex all-the-icons-dired)))
+	 '(perspective-tabs eshell-syntax-highlighting kind-icon all-the-icons-completion nyan-mode eshell-git-prompt python-pytest paredit blackout nano-theme git-timemachine magit focus lsp-ui lox-mode prettier-js perspective typescript-mode tsx-mode modus-themes wgrep imenu-list side-hustle lsp-pyright python-black iceberg-theme web-mode corfu material-theme yafolding yasnippet-snippets use-package undo-tree tron-legacy-theme tree-sitter-langs sublime-themes sqlformat rg restclient rake rainbow-delimiters projectile pdf-view-restore origami org-roam-ui org-drill org-download org-bullets orderless olivetti native-complete multi-vterm marginalia kubel inflections inf-ruby helpful go-mode flycheck exec-path-from-shell evil-vimish-fold evil-leader evil-collection enh-ruby-mode embark-consult doom-themes doom-modeline docker dired-sidebar dashboard bug-hunter balanced-windows auctex all-the-icons-dired)))
+;;; custom.el ends here
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,16 +20,18 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:background nil))))
  '(blamer-face ((t :foreground "#7a88cf" :background nil :height 140 :italic t)))
- '(imenu-list-entry-face-1 ((t (:foreground "white"))))
  '(italic ((t (:foreground "white" :slant italic))))
- '(org-document-title ((t (:inherit default :foreground "#B0CCDC" :underline nil :slant normal :weight bold :height 6.0 :width normal :foundry "nil" :family "ETBembo"))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 2.75))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 1.5))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "ETBembo")))))
-
-;;; custom.el ends here
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold" :height 6.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold" :height 2.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold"))))
+ '(tab-bar ((t (:inherit variable-pitch :foreground "black"))))
+ '(tab-bar-tab ((t (:inherit bold))))
+ '(tab-bar-tab-group-current ((t (:inherit bold :box (:line-width (2 . -2) :color "#000000")))))
+ '(tab-bar-tab-inactive ((t (:box (:line-width (2 . -2) :color "#545454")))))
+ '(tab-line ((t (:inherit modus-themes-ui-variable-pitch :height 0.95)))))
