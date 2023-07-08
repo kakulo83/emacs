3fe6ce7053f26bbfd486cd4e8167358f56b11924
Robert Carter <kakulo83@gmail.com>
2023-07-08 13:57:40 +0200

    use builtin treesitter, add compiled language dylib binaries
    the dylib binaries are used with treesitter for faster language
    syntax parsing

--- config/packages.el
+++ config/packages.el
@@ -68,6 +68,40 @@
 				 (typescript-ts-mode . eglot-ensure)
 				 (sql-mode . eglot-ensure)))
 
+(use-package typescript-ts-mode
+	:config
+	(setq-default typescript-indent-level 4)
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescript-ts-mode)))
+
+; https://www.nathanfurnal.xyz/posts/building-tree-sitter-langs-emacs/
+(setq treesit-language-source-alist
+	'((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
+		(c . ("https://github.com/tree-sitter/tree-sitter-c"))
+		(cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
+		(css . ("https://github.com/tree-sitter/tree-sitter-css"))
+		(go . ("https://github.com/tree-sitter/tree-sitter-go"))
+		(html . ("https://github.com/tree-sitter/tree-sitter-html"))
+		(javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
+		(json . ("https://github.com/tree-sitter/tree-sitter-json"))
+		(lua . ("https://github.com/Azganoth/tree-sitter-lua"))
+		(make . ("https://github.com/alemuller/tree-sitter-make"))
+		(ocaml . ("https://github.com/tree-sitter/tree-sitter-ocaml" "ocaml/src" "ocaml"))
+		(python . ("https://github.com/tree-sitter/tree-sitter-python"))
+		(php . ("https://github.com/tree-sitter/tree-sitter-php"))
+		(typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+		(ruby . ("https://github.com/tree-sitter/tree-sitter-ruby"))
+		(rust . ("https://github.com/tree-sitter/tree-sitter-rust"))
+		(sql . ("https://github.com/m-novikov/tree-sitter-sql"))
+		(toml . ("https://github.com/tree-sitter/tree-sitter-toml"))
+		(zig . ("https://github.com/GrayJack/tree-sitter-zig"))))
+
+; https://www.reddit.com/r/emacs/comments/zqshfy/comment/j0zpwyo/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+(push '(css-mode . css-ts-mode) major-mode-remap-alist)
+(push '(python-mode . python-ts-mode) major-mode-remap-alist)
+(push '(javascript-mode . js-ts-mode) major-mode-remap-alist)
+(push '(js-json-mode . json-ts-mode) major-mode-remap-alist)
+(push '(typescript-mode . typescript-ts-mode) major-mode-remap-alist)
+
 (use-package consult-eglot)
 
 (use-package go-mode
