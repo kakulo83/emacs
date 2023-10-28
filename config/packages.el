b2b95fb618d641cb79127a6d7cb29fac0f458b7f
Robert Carter <kakulo83@gmail.com>
2023-10-28 11:52:23 -0700

    fix corfu and autocomplete

--- config/packages.el
+++ config/packages.el
@@ -76,68 +76,26 @@
 				 (sql-mode . eglot-ensure)))
 
 (use-package corfu
-	; https://kristofferbalintona.me/posts/202202270056/
-	:hook (lsp-completion-mode . my/corfu-setup-lsp)
-	:custom
-	(corfu-auto t)
-	(corfu-auto-delay 0)
-	(corfu-auto-prefix 0)
-	(completion-cycle-threshold nil)
-	(corfu-quit-at-boundary nil)
-  (corfu-separator ?\s)
-	(corfu-quit-no-match 'separator)
-	(corfu-preview-current 'insert)
-	(corfu-preselect-first t)
-	(corfu-echo-documentation nil)
-	(corfu-popupinfo-max-height 14)
-	(lsp-completion-provider :none)
-	:bind
-	(:map corfu-map
-				("C-n" . corfu-next)
-				("C-p" . corfu-previous))
-	:init
-	(global-corfu-mode)
-	(corfu-popupinfo-mode)
-	:config
-	(advice-add 'corfu--setup :after 'evil-normalize-keymaps)
-  (advice-add 'corfu--teardown :after 'evil-normalize-keymaps)
-	(defun my/corfu-setup-lsp ()
-    "Use orderless completion style with lsp-capf instead of the default lsp-passthrough."
-    (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
-          '(orderless)))
-	)
+  :demand t
+  :custom
+  (corfu-auto t)
+  :init
+	(global-corfu-mode))
+
+(use-package orderless
+  :demand t
+  :config
+  (setq completion-styles '(orderless flex)
+        completion-category-overrides '((eglot (styles . (orderless flex))))))
 
 (use-package kind-icon
+  :ensure t
   :after corfu
   :custom
-  (kind-icon-use-icons t)
-  (kind-icon-default-face 'corfu-default) ; Have background color be the same as `corfu' face background
-  (kind-icon-blend-background nil) ; Use midpoint color between foreground and background colors ("blended")?
-  (kind-icon-blend-frac 0.08)
-
-  ;; NOTE 2022-02-05: `kind-icon' depends `svg-lib' which creates a cache
-  ;; directory that defaults to the `user-emacs-directory'. Here, I change that
-  ;; directory to a location appropriate to `no-littering' conventions, a
-  ;; package which moves directories of other packages to sane locations.
-																				;(svg-lib-icons-dir (no-littering-expand-var-file-name "svg-lib/cache/")) ; Change cache dir
+  (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
   :config
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter) ; Enable `kind-icon'
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
-  ;; Add hook to reset cache so the icon colors match my theme
-  ;; NOTE 2022-02-05: This is a hook which resets the cache whenever I switch
-  ;; the theme using my custom defined command for switching themes. If I don't
-  ;; do this, then the backgound color will remain the same, meaning it will not
-  ;; match the background color corresponding to the current theme. Important
-  ;; since I have a light theme and dark theme I switch between. This has no
-  ;; function unless you use something similar
-  (add-hook 'kb/themes-hooks #'(lambda () (interactive) (kind-icon-reset-cache))))
-
-(use-package typescript-ts-mode
-	:config
-	(setq-default typescript-indent-level 4)
-  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescript-ts-mode)))
-
-; https://www.nathanfurnal.xyz/posts/building-tree-sitter-langs-emacs/
 (setq treesit-language-source-alist
    '((bash "https://github.com/tree-sitter/tree-sitter-bash")
 		 (c "https://github.com/tree-sitter/tree-sitter-c")
@@ -203,12 +161,6 @@
 	(setq vertico-resize nil)
 	:init
 	(vertico-mode))
-
-(use-package orderless
-	:init
-	(setq completion-styles '(orderless basic)
-				completion-category-defaults nil
-				completion-category-overrides '((file (styles . (partial-completion))))))
 
 (use-package magit
 	:config
