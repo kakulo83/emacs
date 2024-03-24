4945c27cf82970f54919337802d663d1e806eadf
Robert Carter <kakulo83@gmail.com>
2024-03-24 11:53:26 -0700

    stop tracking tabsession.el

--- tabsession.el
+++
@@ -1,8 +0,0 @@
-;; -*- mode: emacs-lisp; lexical-binding:t; coding: utf-8-emacs; -*-
-;; -------------------------------------------------------------------------
-;; Tabspaces Session File for Emacs
-;; -------------------------------------------------------------------------
-;; Created Sat Mar 23 16:06:12 2024
-
-;; Tabs and buffers:
-(setq tabspaces--session-list '((("/Users/robertcarter/.emacs.d/config/bindings.el" "/Users/robertcarter/.emacs.d/config/packages.el") . "emacs") (nil . "pybrzw")))
