d64277172fcf3c6886d7b3e7d960755064d5f7b5
Robert Carter <kakulo83@gmail.com>
2024-03-30 18:16:14 -0700

    update functions.el with hydra and other utils

--- config/functions.el
+++ config/functions.el
@@ -1,5 +1,8 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
 (defun my/delete-buffer-or-workspace ()
-  "Delete the current buffer or if only buffer left in workspace delete the workspace/tab."
+"Delete the current buffer or workspace/tab."
   (interactive)
   (if (= (length (window-list)) 1)
       (call-interactively (tabspaces-close-workspace))
@@ -28,6 +31,51 @@
          )))
 
 
+(defun copy-filepath-to-clipboard ()
+  "Put the current file name on the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (with-temp-buffer
+        (insert filename)
+        (clipboard-kill-region (point-min) (point-max)))
+      (message filename))))
+
+
+(defun robert/remove-empty-strs-from-list (list)
+  "Remove empty string values from LIST."
+  (--filter (not (string= "" it)) list))
+
+(defvar notes-dir-path "/Users/robertcarter/Notes/org-roam-notes/")
+(defvar rg-drill-cmd "rg -l '.*drill.*' ")
+
+(defun robert/org-files-by-tag (tag)
+  "Function to generate space separated string list of org files by TAG."
+  (cd notes-dir-path)
+  (let ((rg-tag-command (concat "rg -l 'tags:.*'" tag)))
+    (string-join (split-string (shell-command-to-string rg-tag-command) "\n") " ")))
+
+(defun robert/get-org-files-for-topic (tag)
+  "Function to generate list of files from TAG."
+  (interactive)
+                                        ; rg -l '^#\+tags:.*database.*' | xargs rg -l '.*drill.*'
+  (cd notes-dir-path)
+                                        ; bind list of org files that have tag
+  (let ((files-by-tag (robert/org-files-by-tag tag)))
+                                        ; filter list further with onlly files that have drill items
+    (split-string (shell-command-to-string (concat rg-drill-cmd files-by-tag)) "\n")))
+
+(defun robert/drill-by-topic ()
+  "Wrapper function on org-drill to invoke against a list of files from TOPIC."
+  (interactive)
+  (let* ((topic (read-string "Enter subject to drill: "))
+	 (files (robert/get-org-files-for-topic topic)))
+    (setq org-drill-scope (robert/remove-empty-strs-from-list files))
+    (org-drill)))
+
+
 ;; Results of Embark Collection are displayed by invoking the compile-goto-error function.
 ;; Advise this function to do `display-buffer-in-side-window` instead
 (defadvice compile-goto-error (around my-compile-goto-error activate)
@@ -40,23 +88,12 @@
   ("g" text-scale-increase "in")
   ("l" text-scale-decrease "out"))
 
+(defhydra hydra-eglot ()
+  "eglot"
+  ("d" xref-find-definitions "xref definitions")
+  ("r" xref-find-references "xref references")
+  ("=" eglot-format-buffer "format")
+  ("a" eglot-code-actions "apply code action"))
 
-(defhydra hydra-magit ()
-   "magit"
-   ("s" magit-status "status")
-   ("b" magit-blame "blame")
-   ("l" magit-log "log")
-   ("c" magit-checkout "checkout")
-   ("C" magit-commit "commit")
-   ("P" magit-push "push")
-   ("F" magit-fetch "fetch")
-   ("M" magit-merge "merge")
-   ("R" magit-rebase "rebase")
-   ("t" magit-tag "tag")
-   ("T" magit-tag-delete "delete tag")
-   ("r" magit-revert "revert")
-   ("d" magit-diff "diff")
-   ("D" magit-diff-buffer-file "diff buffer")
-   ("f" magit-find-file "find file")
-   ("L" magit-list-repositories "list repositories")
-   ("q" nil "quit"))
+(provide 'functions)
+;;; functions.el ends here
