f8b7548dbe512eb5756ea5b0ef0995d857313531
Robert Carter <kakulo83@gmail.com>
2024-04-06 22:04:25 -0700

    update custom.el;  simplify org faces

--- custom.el
+++ custom.el
@@ -1,22 +1,42 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-    '("702d0136433ca65a7aaf7cc8366bd75e983fe02f6e572233230a528f25516f7e" "b216e9b72dc8c2b702e4fcfd3c0af2d73c87eba46fd4db824ddb50863447d6a9" "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0" "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5" "7af2a6fcd1e743d165c58fd95d20b46c2d96d9873ab67fc9371bdc8fda463de7" default))
+    '("702d0136433ca65a7aaf7cc8366bd75e983fe02f6e572233230a528f25516f7e"
+       "b216e9b72dc8c2b702e4fcfd3c0af2d73c87eba46fd4db824ddb50863447d6a9"
+       "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0"
+       "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5"
+       "7af2a6fcd1e743d165c58fd95d20b46c2d96d9873ab67fc9371bdc8fda463de7"
+       default))
  '(package-selected-packages
-    '(org-roam-ui hide-mode-line doom-modeline org-drill websocket simple-httpd org-roam flycheck format-all hydra vterm-toggle multi-vterm vterm olivetti nvm restclient org-bullets nano-theme modus-themes better-jumper undo-tree eshell-toggle editorconfig evil-collection copilot consult marginalia all-the-icons-dired all-the-icons embark helpful lambda-themes ef-themes use-package-ensure-system-package magit evil dired-sidebar)))
+    '(all-the-icons all-the-icons-dired better-jumper consult copilot
+       dired-sidebar disaster doom-modeline editorconfig ef-themes
+       embark eshell-toggle evil evil-collection flycheck format-all
+       helpful hide-mode-line hydra lambda-themes magit marginalia
+       modus-themes multi-vterm nano-theme nvm olivetti org-bullets
+       org-drill org-roam org-roam-ui restclient simple-httpd
+       undo-tree use-package-ensure-system-package vterm vterm-toggle
+       websocket yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold" :height 2.75))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold" :height 1.5))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold" :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold")))))
+ ;; example:  '(org-level-1 ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold" :height 2.75))))
+ '(org-level-1 ((t (:inherit default :weight bold  :height 2.75))))
+ '(org-level-2 ((t (:inherit default :weight bold  :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold  :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold  :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold ))))
+ '(org-level-6 ((t (:inherit default :weight bold ))))
+ '(org-level-7 ((t (:inherit default :weight bold ))))
+ '(org-level-8 ((t (:inherit default :weight bold )))))
+
+(provide 'custom)
+;;; custom.el ends here
+
