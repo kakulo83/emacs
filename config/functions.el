5926c8d7cfb5eb70080179c2190bfe7639f68e2c
Robert Carter <kakulo83@gmail.com>
2024-04-29 16:24:48 -0700

    add useful org-toggle functions

--- config/functions.el
+++ config/functions.el
@@ -205,6 +205,30 @@
   (find-file "~/Notes/org-roam-notes/")
   (call-interactively 'project-find-file))
 
+(defun org-hide-properties ()
+  "Hide all 'org-mode' headline property drawers in buffer.  Could be slow if it has a lot of overlays."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward
+            "^ *:properties:\n\\( *:.+?:.*\n\\)+ *:end:\n" nil t)
+      (let ((ov_this (make-overlay (match-beginning 0) (match-end 0))))
+        (overlay-put ov_this 'display "")
+        (overlay-put ov_this 'hidden-prop-drawer t))))
+  (put 'org-toggle-properties-hide-state 'state 'hidden))
+
+(defun org-show-properties ()
+  "Show all 'org-mode' property drawers hidden by org-hide-properties."
+  (interactive)
+  (remove-overlays (point-min) (point-max) 'hidden-prop-drawer t)
+  (put 'org-toggle-properties-hide-state 'state 'shown))
+
+(defun org-toggle-properties ()
+  "Toggle visibility of property drawers."
+  (interactive)
+  (if (eq (get 'org-toggle-properties-hide-state 'state) 'hidden)
+      (org-show-properties)
+    (org-hide-properties)))
 
 (provide 'functions)
 ;;; functions.el ends here
