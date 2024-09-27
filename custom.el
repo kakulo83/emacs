1de25bb64700bdcc9c38646477cb63d0441108b5
Robert Carter <kakulo83@gmail.com>
2024-09-26 22:37:25 -0700

    remove custom.el crap

--- custom.el
+++ custom.el
@@ -6,66 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-    '("702d0136433ca65a7aaf7cc8366bd75e983fe02f6e572233230a528f25516f7e"
-       "b216e9b72dc8c2b702e4fcfd3c0af2d73c87eba46fd4db824ddb50863447d6a9"
-       "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0"
-       "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5"
-       "7af2a6fcd1e743d165c58fd95d20b46c2d96d9873ab67fc9371bdc8fda463de7"
-       default))
- '(org-bullets-bullet-list '("🌺" "🌸" "🌼" "🌿" "🍀"))
- '(package-selected-packages
-    '(ace-window aggressive-indent all-the-icons-completion
-       all-the-icons-dired all-the-icons-ibuffer auctex badwolf-theme
-       balanced-windows better-jumper blackout bug-hunter cape
-       company-box consult-dir consult-eglot consult-yasnippet copilot
-       dashboard deadgrep dired-sidebar disaster doom-modeline
-       doom-themes ef-themes eglot-booster embark-consult
-       eshell-syntax-highlighting eshell-toggle evil-collection
-       evil-leader flycheck focus format-all fussy git-timemachine
-       go-mode gotest gruvbox-theme helpful hide-mode-line
-       highlight-symbol hydra-posframe inf-ruby json-reformat
-       kind-icon lox-mode magit marginalia modus-themes multi-vterm
-       nano-theme nodejs-repl nord-theme nvm nyan-mode olivetti
-       orderless org-bullets org-download org-drill org-roam-ui
-       perspective planet-theme prettier-js projectile python-black
-       pyvenv rainbow-delimiters rbenv restclient rg simple-modeline
-       sqlformat sublime-themes tabspaces tron-legacy-theme undo-tree
-       vertico-posframe vterm-toggle web-mode yasnippet-capf
-       yasnippet-snippets))
- '(package-vc-selected-packages
-    '((org-margin :url "https://github.com/rougier/org-margin")
-       (hydra-posframe :url
-	 "https://github.com/Ladicle/hydra-posframe" :branch "master")
-       (eglot-booster :vc-backend Git :url
-	 "https://github.com/jdtsmith/eglot-booster"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background nil))))
- '(fixed-pitch ((t (:family "Fira Code Retina" :height 160))))
- '(org-document-info ((t (:foreground "dark orange"))))
- '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-title ((t (:inherit default :weight bold :foreground "#B0CCDC" :font "Hack Nerd Font Bold" :height 6.0 :underline nil))))
- '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
- '(org-level-1 ((t (:inherit default :weight bold :height 2.75))))
- '(org-level-2 ((t (:inherit default :weight bold :height 1.5))))
- '(org-level-3 ((t (:inherit default :weight bold :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold))))
- '(org-level-6 ((t (:inherit default :weight bold))))
- '(org-level-7 ((t (:inherit default :weight bold))))
- '(org-level-8 ((t (:inherit default :weight bold))))
- '(org-link ((t (:foreground "deep sky blue" :underline t))))
- '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-property-value ((t (:inherit fixed-pitch))))
- '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
- '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
- '(variable-pitch ((t (:family "ETBembo" :height 180 :weight thin)))))
+)
 
 (provide 'custom)
 ;;; custom.el ends here
